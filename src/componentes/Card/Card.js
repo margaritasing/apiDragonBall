5e1feadf4709bd6b537d7f8e25b071a26c2bb3fb
margaritasing <libiamfreitesm@gmail.com>
2022-09-16 18:29:03 -0300

    agrego cambios a la card

--- src/componentes/Card/Card.js
+++ src/componentes/Card/Card.js
@@ -25,21 +25,24 @@ const Card = () => {
     }
 
   return (
-    <div className="row my-2 mx-2">
+    <div className="row">
   {starW.map((prueba) => (
         <div className="col-3" key={prueba.id}>
         <div className="card my-2">
-             <img src={prueba.imageUrl}  className="card-img-top" alt="..."/> 
+            <div className='card-img'>
+               <img src={prueba.imageUrl} style={{ width:"240px", height:"300px" }} className="card-img-top" alt="..."/> 
+            
+            </div>
          {  /*   <button className="favorito"
              onClick={props.addOrRemoveFromFavs} 
              data-movie-id={prueba.id}>🖤
   </button>   */   }      
            <div className="card-body text-center">
               <h5 className="card-title">{Capitalize(prueba.name)}</h5>
-              <p className="card-text">{Capitalize(prueba.originplanet)}</p>
-              <p className="card-text">{Capitalize(prueba.role)}</p>
-              <p className="card-text">{Capitalize(prueba.specie)}</p>
-              <p className="card-text">{Capitalize(prueba.status)}</p>
+              <p className="card-text">Planeta: {Capitalize(prueba.originplanet)}</p>
+              <p className="card-text">Rol: {Capitalize(prueba.role)}</p>
+              <p className="card-text">Especie:  {Capitalize(prueba.specie)}</p>
+              <p className="card-text"> Status: {Capitalize(prueba.status)}</p>
             { /*  <Link to={`/detalle?id=${prueba.id}`} className="btn btn-dark" >View details</Link> */}
           </div>
         </div>     
